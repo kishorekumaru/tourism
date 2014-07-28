c6195b095ee974b878ee6461041d76145dc691d9
kishorekumaru <kishorekumaru@gmail.com>
2014-07-28 11:13:40 +0530

    Update mainapp.js

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,4 +1,18 @@
-// JavaScript Document
+/**
+ * @author Kishore Kumar Unnikrishnan
+ * Created Date - 05-05-2014 
+ 
+-- Client : Glacier Holidays--
+Version : 1.0
+Updated on 10 July 2014 
+
+-- About
+	
+Main Module File
+	
+
+*/
+
 var app = angular.module("glacierHolidays", []);
 
 
